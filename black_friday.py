976324ebd0e2c3eb333c5b6838add497a9b8895d
joshuaPhong <110524932+joshuaPhong@users.noreply.github.com>
2023-08-16 13:32:45 +1200

    initial commit.  black friday class and function defined

---
+++ black_friday.py
@@ -0,0 +1,21 @@
+class BlackFriday:
+    """
+
+    """
+    def __init__(self):
+        """
+
+        """
+        self._discount = 50
+
+
+    def apply_discount(self, cart_items, cart_total ):
+        """
+
+        :param cart_items: the total number of items in a customer's cart
+        :type cart_items: int
+        :param cart_total: the total value of the customer's cart items
+        :type cart_total: float
+        :return: the
+        :rtype: float
+        """
