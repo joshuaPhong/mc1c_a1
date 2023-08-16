efe042bb7b35ba010db5e2e1c9d0db62e3eea199
joshuaPhong <110524932+joshuaPhong@users.noreply.github.com>
2023-08-16 20:08:23 +1200

    added doc strings to BlackFriday. blocked out test class created an instance in main

--- black_friday.py
+++ black_friday.py
@@ -1,21 +1,45 @@
 class BlackFriday:
     """
+    A class for XClouds black friday sale.
+    :ivar _black_friday_discount: The discount applied if the conditions are met.
+        A private variable, for Black Friday use only.
+    :type _black_friday_discount: float
+
+    Methods:
+
+    Example:
+
 
     """
+
     def __init__(self):
         """
+        The constructor for BlackFriday class.
+        Initializes the Black Friday instance when called.
+        Has a default, private variable; _black_friday_discount: float
 
         """
-        self._discount = 50
 
+        # a class only / private variable to define the discount's value
+        self._black_friday_discount = 50.0
 
-    def apply_discount(self, cart_items, cart_total ):
+    def apply_discount(self, cart_items, cart_total):
         """
-
-        :param cart_items: the total number of items in a customer's cart
+        Apply the black friday discount if:
+        cart_items >= 5 AND cart_total >= 200.
+        :param cart_items: The total number of items in a customer's cart.
         :type cart_items: int
-        :param cart_total: the total value of the customer's cart items
+        :param cart_total: The total value of the customer's cart items.
         :type cart_total: float
-        :return: the
+
+        :return: cart_total - discount; If discount is applied, otherwise
+        cart_total
         :rtype: float
+
+        Example:
+
         """
+        if cart_items >= 5 and cart_total >= 200.0:
+            return cart_total - self._black_friday_discount
+        else:
+            return cart_total
---
+++ test_black_friday.py
@@ -0,0 +1,17 @@
+import unittest
+
+
+class TestBlackFriday(unittest.TestCase):
+    """
+
+    """
+    def test_apply_discount(self):
+        """
+
+        :return:
+        """
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
