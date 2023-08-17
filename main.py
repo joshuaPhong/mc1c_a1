24e7bc03924dbb019a28561f97dc82a572c63225
joshuaPhong <110524932+joshuaPhong@users.noreply.github.com>
2023-08-17 14:33:45 +1200

    no real changes. white box testing, changing params

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ if __name__ == '__main__':
     # create an instance
     black_friday = BlackFriday()
     # access the function
-    print(black_friday.apply_discount(-1.09, 23.0))
+    print(black_friday.apply_discount("4.9999999999", 23.0))
     print(black_friday.apply_discount(1, -203.0))
-    print(black_friday.apply_discount(5,199.99 ))
+    print(black_friday.apply_discount(4.9,200.0 ))
     print(black_friday.apply_discount(5,
                                       200000000000000000000000000000000000000000000000000.01))
