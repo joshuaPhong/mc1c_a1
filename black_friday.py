dc626549080456aa5c35297610fd1ea2a5e1fd80
joshuaPhong <110524932+joshuaPhong@users.noreply.github.com>
2023-08-24 17:00:23 +1200

    no change

--- black_friday.py
+++ black_friday.py
@@ -1,3 +1,16 @@
+# SOURCES:
+# https://realpython.com/documenting-python-code/
+# https://stackoverflow.com/questions/41052221/what-is-the-difference-between-var-cvar-and-ivar-in-pythons-sphinx
+# https://www.jetbrains.com/help/pycharm/documenting-source-code.html
+# https://www.sphinx-doc.org/en/master/usage/restructuredtext/domains.html#info-field-lists
+# https://chat.openai.com/ ? i want to change the exception type as I am going
+# to remove debug mode: It was at this time a blanket Exception as err
+# ValueError Exception: The exception type is changed to ValueError,
+# which is more appropriate for handling conversion issues.
+# for the read me I looked at markdown https://commonmark.org/help/
+# https://www.geeksforgeeks.org/python-oops-concepts/
+
+
 class BlackFriday:
     """
     A class for XClouds black friday sale.
