cd4b0a5471b8cf4c56a74424c102e3e33b152e4d
joshuaPhong <110524932+joshuaPhong@users.noreply.github.com>
2023-08-25 12:34:12 +1200

    main has a copy of all sources used

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-# SOURCES:
+
+# SOURCES: for black_friday and test-black_friday
 # https://realpython.com/documenting-python-code/
 # https://stackoverflow.com/questions/41052221/what-is-the-difference-between-var-cvar-and-ivar-in-pythons-sphinx
 # https://www.jetbrains.com/help/pycharm/documenting-source-code.html
@@ -9,6 +10,19 @@
 # which is more appropriate for handling conversion issues.
 # for the read me I looked at markdown https://commonmark.org/help/
 # https://www.geeksforgeeks.org/python-oops-concepts/
+# gpt 23/8/23 ? I want to improve my documentation on a unit test.
+# Supplied current version of test one.
+# Response. expand description, explain test steps, change return type
+# (tests have none, was true or false), add an exception, and enhance the
+# error message.
+# it also suggested using the given, when, then model
+# https://pythontest.com/strategy/given-when-then-2/
+# https://softwareengineering.stackexchange.com/questions/367378/should-given-when-and-then-comments-be-included-in-unit-tests
+# I will apply this to all the tests.
+# I have chosen to implement each test separately for readability and
+# modularity. Each one can be easily modified should the need arise (say a
+# change to the function apply_discount()), without
+# affecting the others. And the test steps and reasons are clearer.
 
 
 # import module
